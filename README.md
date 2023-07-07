5496e25c5b224cb9c4870ac9ff509becebbf06fd
danielecas <daniele.casiello@hotmail.it>
2023-07-07 15:34:47 +0200

    wip

---
+++ README.md
@@ -0,0 +1,32 @@
+# Azure DevOps Extension: Mermaid Viewer
+
+## Overview
+
+This Azure DevOps Extension allows to render your .mmd files on your Azure DevOps Repos.
+
+## Usage
+
+Install this extension to your Azure DevOps Organization.
+
+Go to Repos:
+- in Files: if you select a .mmd file you can find a new Preview tab
+- in Pull Requests: if you select a .mmd file you can find a Raw Content/Preiew toggle button
+
+## Run Locally
+
+    npm install
+    npm install -g tfx-cli
+    tfx extension init
+
+## For contributors
+
+Contributions are welcome, Feel free to create a Pull Request or Contact me to give your suggestions 
+
+```mermaid
+sequenceDiagram
+    Alice ->> Bob: Hello Bob, how are you?
+    Bob-->Alice: Hi!
+```
+
+
+<a href="https://www.flaticon.com/free-icons/mermaid" title="mermaid icons">Mermaid icons created by Freepik - Flaticon</a>
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import * as SDK from "azure-devops-extension-sdk";
+import Mermaid from "mermaid";
+
+await (async function() : Promise<void> {
+
+    SDK.init({ loaded: false })
+    Mermaid.initialize({ startOnLoad: false });
+
+    await SDK.ready();
+
+    SDK.register("mermaid_viewer", function (context) {
+            return mermaidViewer;
+    });
+
+    SDK.notifyLoadSucceeded();
+
+
+    var mermaidViewer = (function () {
+        "use strict";
+        return {
+            renderContent: function(rawContent, options) {
+                var rendered = document.getElementById('viewer-content-display');
+
+                var graphDefinition = rawContent;
+                Mermaid.mermaidAPI.render('graphDiv', graphDefinition).then(({ svg, bindFunctions }) => {
+                    rendered.innerHTML = svg;
+                    bindFunctions?.(rendered);
+                });
+            }
+        };
+    }());
+
+}());
